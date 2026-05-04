ea717a81e2bbd0f1f7f8bcca1054b1e6588ca1ae
martinreta4-netizen <martinreta4@gmail.com>
2026-05-04 10:16:43 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
-const CACHE_NAME = "seguridad-escolar-v1";
+const CACHE_NAME = "seguridad-escolar-v2";
 
 const urlsToCache = [
-  "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
@@ -11,12 +10,32 @@ const urlsToCache = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
